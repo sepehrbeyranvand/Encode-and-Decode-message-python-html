7e0905dd95656001957af95872ac07b1a2ad7173
sepehrbeyranvand <sepehr7797@gmail.com>
2024-01-23 01:54:46 +0330

    Change a optional name

--- hideit.py
+++ hideit.py
@@ -1,5 +1,5 @@
 from stegano import lsb #Import stegano library
-secret = lsb.hide("Iran.png", "SepehrBeiranvand!") #choose your image and your text
+secret = lsb.hide("Image.png", "SepehrBeiranvand!") #choose your image and your text
 secret.save("Name.png") #set a name or your new image file which contains a secret message
 
 #SepehrBeiranvand
